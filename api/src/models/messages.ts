9d909a785349e3d09656318d922854b3b44ec76a
MHammadYou <MHammadY2.0@gmail.com>
2023-04-29 23:16:28 +0500

    Added MessagesSchema

--- api/src/models/messages.ts
+++ api/src/models/messages.ts
@@ -3,6 +3,13 @@ import { Schema, Document, model } from "mongoose";
 
 interface MessagesInterface extends Document {
     body: string,
-    conversation: Schema.Types.ObjectId,
-    user: Schema.Types.ObjectId
+    user: Schema.Types.ObjectId,
+    conversation: Schema.Types.ObjectId
 }
+
+
+const Messages = new Schema<MessagesInterface>({
+    body: String,
+    user: { type: Schema.Types.ObjectId, ref: 'Users'},
+    conversation: { type: Schema.Types.ObjectId, ref: 'Conversations' }
+})
