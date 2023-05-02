9144674a0ee2a794adac4cb6223a4f77f19cb58e
MHammadYou <MHammadY2.0@gmail.com>
2023-05-03 00:03:06 +0500

    Added ConversationsSchema

--- api/src/models/conversations.ts
+++ api/src/models/conversations.ts
@@ -5,3 +5,9 @@ interface ConversationsInterface extends Document {
     messages: Schema.Types.ObjectId;
     users: Schema.Types.ObjectId;
 }
+
+
+const ConversationsSchema = new Schema<ConversationsInterface>({
+    messages: { type: Schema.Types.ObjectId, ref: 'Messages'},
+    users: { type: Schema.Types.ObjectId, ref: 'Users'}
+})
