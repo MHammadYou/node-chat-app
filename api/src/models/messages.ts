ad35bc45b588a6edffbf832e5e6e70aeb2813bb7
MHammadYou <MHammadY2.0@gmail.com>
2023-05-15 00:25:16 +0500

    Applied formatting

--- api/src/models/messages.ts
+++ api/src/models/messages.ts
@@ -1,20 +1,17 @@
 import { Schema, Document, model } from "mongoose";
 
-
 interface MessagesInterface extends Document {
-    body: string,
-    user: Schema.Types.ObjectId,
-    conversation: Schema.Types.ObjectId
+  body: string;
+  user: Schema.Types.ObjectId;
+  conversation: Schema.Types.ObjectId;
 }
 
-
 const messagesSchema = new Schema<MessagesInterface>({
-    body: String,
-    user: { type: Schema.Types.ObjectId, ref: 'Users'},
-    conversation: { type: Schema.Types.ObjectId, ref: 'Conversations' }
-})
+  body: String,
+  user: { type: Schema.Types.ObjectId, ref: "Users" },
+  conversation: { type: Schema.Types.ObjectId, ref: "Conversations" },
+});
 
-
-const Messages = model<MessagesInterface>('Messages', messagesSchema);
+const Messages = model<MessagesInterface>("Messages", messagesSchema);
 
 export default Messages;
