e77d10223f327db611c775df58853d8a0511e092
MHammadYou <MHammadY2.0@gmail.com>
2023-05-16 00:28:02 +0500

    Applied formatting

--- api/src/models/users.ts
+++ api/src/models/users.ts
@@ -1,22 +1,19 @@
 import { Schema, Document, model } from "mongoose";
 
-
 interface UsersInterface extends Document {
-  username: string,
-  email: string,
-  password: string,
+  username: string;
+  email: string;
+  password: string;
   // TODO: Add later
   // picture: string
 }
 
-
 const usersSchema = new Schema<UsersInterface>({
   username: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
-  password: { type: String, unique: true, required: true }
+  password: { type: String, unique: true, required: true },
 });
 
-
-const Users = model<UsersInterface>('Users', usersSchema);
+const Users = model<UsersInterface>("Users", usersSchema);
 
 export default Users;
