e05095058d652848285d3f0cdc4cad3e00c12346
MHammadYou <MHammadY2.0@gmail.com>
2023-05-18 00:37:34 +0500

    Added 404 route

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 
-const handleRoutes = (app: express.Express) => {};
+const handleRoutes = (app: express.Express) => {
+  app.use((_req, res) => {
+    res.status(404).send("Not found");
+  });
+};
 
 export default handleRoutes;
