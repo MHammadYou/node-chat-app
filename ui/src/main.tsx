2bcd579e9938e5c5702c8325bce590390427186b
MHammadYou <MHammadY2.0@gmail.com>
2023-06-13 19:11:17 +0500

    Apply ThemeProvider

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from "react-redux";
 
 import App from "./App.tsx";
 import store from "./store";
+import ThemeProvider from "./theme/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
