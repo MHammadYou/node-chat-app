9100cc4904454e9dd35f0bd23ae014ee0ccbdfa9
MHammadYou <MHammadY2.0@gmail.com>
2023-06-30 19:59:11 +0500

    Add StyledAppTitle

--- ui/src/components/nav/AppTitle.tsx
+++ ui/src/components/nav/AppTitle.tsx
@@ -1,12 +1,15 @@
-import { Typography } from "@mui/material";
+import { styled, Typography, TypographyProps } from "@mui/material";
 import { Link } from "react-router-dom";
+
+const StyledAppTitle = styled(Typography)<TypographyProps>(({ theme }) => ({
+  color: theme.palette.text.primary,
+  padding: "0.5rem",
+}));
 
 const AppTitle: React.FC = () => {
   return (
     <Link to="/" state={{ TextDecoder: "none" }}>
-      <Typography variant="h5" sx={{ padding: 1 }}>
-        Chat App
-      </Typography>
+      <StyledAppTitle variant="h5">Chat App</StyledAppTitle>
     </Link>
   );
 };
