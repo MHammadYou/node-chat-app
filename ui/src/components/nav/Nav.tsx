6fb4162a755c92a5406cfd6dd5e845a9727e36ae
MHammadYou <MHammadY2.0@gmail.com>
2023-07-01 20:06:08 +0500

    Add NavTitle

--- ui/src/components/nav/Nav.tsx
+++ ui/src/components/nav/Nav.tsx
@@ -1,7 +1,12 @@
 import { AppBar } from "@mui/material";
+import AppTitle from "./AppTitle";
 
 const Nav: React.FC = () => {
-  return <AppBar elevation={1} position="static"></AppBar>;
+  return (
+    <AppBar elevation={1} position="static">
+      <AppTitle />
+    </AppBar>
+  );
 };
 
 export default Nav;
