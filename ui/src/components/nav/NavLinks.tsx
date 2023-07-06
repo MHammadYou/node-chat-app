ed59236e95d9bcfd5c6eb85f6adcc68780fbe700
MHammadYou <MHammadY2.0@gmail.com>
2023-07-07 00:23:29 +0500

    Add Temporary navlinks

--- ui/src/components/nav/NavLinks.tsx
+++ ui/src/components/nav/NavLinks.tsx
@@ -1,7 +1,14 @@
 import { Box } from "@mui/material";
+import NavLink from "./NavLink";
 
 const NavLinks: React.FC = () => {
-  return <Box>NavLinks</Box>;
+  return (
+    <Box>
+      {/* TODO: Add proper routes later */}
+      <NavLink to={"signup"} text={"Sign up"} />
+      <NavLink to={"login"} text={"Sign up"} />
+    </Box>
+  );
 };
 
 export default NavLinks;
