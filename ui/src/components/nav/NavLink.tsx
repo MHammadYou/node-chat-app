d2aac2b9853050c0b039284ed7330126ce11db46
MHammadYou <MHammadY2.0@gmail.com>
2023-07-16 01:08:51 +0500

    Update NavLink, text -> children

--- ui/src/components/nav/NavLink.tsx
+++ ui/src/components/nav/NavLink.tsx
@@ -2,11 +2,11 @@ import { Link } from "../../lib";
 
 type Props = {
   to: string;
-  text: string;
+  children: React.ReactNode;
 };
 
-const NavLink: React.FC<Props> = ({ to, text }) => {
-  return <Link to={to}>{text}</Link>;
+const NavLink: React.FC<Props> = ({ to, children }) => {
+  return <Link to={to}>{children}</Link>;
 };
 
 export default NavLink;
