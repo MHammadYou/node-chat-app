7543177b117e37caaaaa5d8f3d005446e91371ec
MHammadYou <MHammadY2.0@gmail.com>
2023-07-17 01:09:50 +0500

    Update NavLinks

--- ui/src/components/nav/NavLinks.tsx
+++ ui/src/components/nav/NavLinks.tsx
@@ -5,8 +5,8 @@ const NavLinks: React.FC = () => {
   return (
     <Box>
       {/* TODO: Add proper routes later */}
-      <NavLink to={"signup"} text={"Sign up"} />
-      <NavLink to={"login"} text={"Sign up"} />
+      <NavLink to={"signup"}>Signup</NavLink>
+      <NavLink to={"login"}>Login</NavLink>
     </Box>
   );
 };
