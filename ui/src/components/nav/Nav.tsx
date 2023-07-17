7111cf6a3dd8cebc50e72b6c26f3cd44a227a3b9
MHammadYou <MHammadY2.0@gmail.com>
2023-07-18 01:11:55 +0500

    Update Nav to include NavLinks

--- ui/src/components/nav/Nav.tsx
+++ ui/src/components/nav/Nav.tsx
@@ -1,10 +1,13 @@
 import { AppBar } from "@mui/material";
+
 import AppTitle from "./AppTitle";
+import NavLinks from "./NavLinks";
 
 const Nav: React.FC = () => {
   return (
     <AppBar elevation={1} position="static">
       <AppTitle />
+      <NavLinks />
     </AppBar>
   );
 };
