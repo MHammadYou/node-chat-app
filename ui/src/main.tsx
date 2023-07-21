7a1406fa01beb8e81d6cef0d301d040e7899a2ff
MHammadYou <MHammadY2.0@gmail.com>
2023-07-22 01:20:07 +0500

    Update store absolute import

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
+import store from "store/";
+
+import ThemeProvider from "theme/ThemeProvider";
 import App from "./App.tsx";
-import store from "./store";
-import ThemeProvider from "./theme/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
