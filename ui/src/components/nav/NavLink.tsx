8cc26142a6fe360ec5044e587b29bc1fdc42caf5
MHammadYou <MHammadY2.0@gmail.com>
2023-08-11 00:55:55 +0500

    Add StyledNavLink

--- ui/src/components/nav/NavLink.tsx
+++ ui/src/components/nav/NavLink.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { styled, Typography, TypographyProps } from "@mui/material";
 import { Link } from "lib/";
 
 type Props = {
@@ -6,10 +6,16 @@ type Props = {
   children: string;
 };
 
+const StyledNavLink = styled(Typography)<TypographyProps>(({ theme }) => ({
+  color: theme.palette.text.primary,
+  padding: "0 0.5rem",
+  textTransform: "capitalize",
+}));
+
 const NavLink: React.FC<Props> = ({ to, children }) => {
   return (
     <Link to={to}>
-      <Typography>{children}</Typography>
+      <StyledNavLink>{children}</StyledNavLink>
     </Link>
   );
 };
