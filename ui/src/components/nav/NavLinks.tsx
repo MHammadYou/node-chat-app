2e4fe9c7cdc93f53df535490099434cf20c45a80
MHammadYou <MHammadY2.0@gmail.com>
2023-08-18 20:25:02 +0500

    Add StyledNavLinks

--- ui/src/components/nav/NavLinks.tsx
+++ ui/src/components/nav/NavLinks.tsx
@@ -1,13 +1,20 @@
-import { Box } from "@mui/material";
+import { styled, Box, BoxProps } from "@mui/material";
 import NavLink from "./NavLink";
+
+const StyledNavLinks = styled(Box)<BoxProps>(() => ({
+  flex: 1,
+  display: "flex",
+  justifyContent: "end",
+  alignItems: "center",
+}));
 
 const NavLinks: React.FC = () => {
   return (
-    <Box>
+    <StyledNavLinks>
       {/* TODO: Add proper routes later */}
       <NavLink to={"signup"}>Signup</NavLink>
       <NavLink to={"login"}>Login</NavLink>
-    </Box>
+    </StyledNavLinks>
   );
 };
 
