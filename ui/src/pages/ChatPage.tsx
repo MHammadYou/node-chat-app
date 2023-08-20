722c4adeee8f6d76e45a87a7b44d993b051ad012
MHammadYou <MHammadY2.0@gmail.com>
2023-08-20 18:20:35 +0500

    Add ChatPage

---
+++ ui/src/pages/ChatPage.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const ChatPage: React.FC = () => {
+  return (
+    <Box>
+      <Typography>Chat Page</Typography>
+    </Box>
+  );
+};
+
+export default ChatPage;
