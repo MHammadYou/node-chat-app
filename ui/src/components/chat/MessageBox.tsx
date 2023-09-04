f92df78fc501d0933812f96e94e4d201c3234e0e
MHammadYou <MHammadY2.0@gmail.com>
2023-09-04 19:06:20 +0500

    Add MessageBox

---
+++ ui/src/components/chat/MessageBox.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const MessageBox: React.FC = () => {
+  return (
+    <Box>
+      <Typography>Type message here</Typography>
+    </Box>
+  );
+};
+
+export default MessageBox;
