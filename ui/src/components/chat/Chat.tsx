1246d100657641385193825001e3cf3a4a6c5a8f
MHammadYou <MHammadY2.0@gmail.com>
2023-09-14 22:42:16 +0500

    Add NewMessage to Chat

--- ui/src/components/chat/Chat.tsx
+++ ui/src/components/chat/Chat.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import Messages from "./Messages";
+import NewMessage from "./NewMessage";
 
 const Chat: React.FC = () => {
   return (
     <Box>
       <Messages />
+      <NewMessage />
     </Box>
   );
 };
