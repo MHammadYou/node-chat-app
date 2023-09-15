6ba2f6c6181f1d136751ff0b833d6fc988033188
MHammadYou <MHammadY2.0@gmail.com>
2023-09-15 22:48:04 +0500

    Add TextField to MessageInput

--- ui/src/components/chat/MessageInput.tsx
+++ ui/src/components/chat/MessageInput.tsx
@@ -1,7 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 
 const MessageInput: React.FC = () => {
-  return <Box>Message text here;</Box>;
+  return (
+    <Box>
+      <TextField size="small" />
+    </Box>
+  );
 };
 
 export default MessageInput;
