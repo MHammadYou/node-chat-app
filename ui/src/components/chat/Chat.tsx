436e57bfbbf2bfcc826a684ef36f2a7a2de6e03a
MHammadYou <MHammadY2.0@gmail.com>
2023-09-17 22:55:30 +0500

    Add StyledChat

--- ui/src/components/chat/Chat.tsx
+++ ui/src/components/chat/Chat.tsx
@@ -1,13 +1,21 @@
-import { Box } from "@mui/material";
+import { styled, Box, BoxProps } from "@mui/material";
+
 import Messages from "./Messages";
 import NewMessage from "./NewMessage";
 
+const StyledChat = styled(Box)<BoxProps>(() => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
 const Chat: React.FC = () => {
   return (
-    <Box>
+    <StyledChat>
       <Messages />
       <NewMessage />
-    </Box>
+    </StyledChat>
   );
 };
 
