bea867e62ac360b0702be728be4ef81e7c2ea9a2
MHammadYou <MHammadY2.0@gmail.com>
2023-09-30 02:34:02 +0500

    Move NewMessage components to new-message dir

--- ui/src/components/messages/index.ts
+++ ui/src/components/messages/index.ts
@@ -1,4 +1,4 @@
 export { default as Messages } from "./Messages";
-export { default as NewMessage } from "./NewMessage";
+export { default as NewMessage } from "./new-message";
 
 export { default } from "./Messages";
