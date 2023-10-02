8b6583ee9b311f81e063e8a8ca82d8608d67786d
MHammadYou <MHammadY2.0@gmail.com>
2023-10-02 18:33:09 +0500

    Add LoginPage

---
+++ ui/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const LoginPage: React.FC = () => {
+  return (
+    <Box>
+      <Typography>Login Page</Typography>
+    </Box>
+  );
+};
+
+export default LoginPage;
