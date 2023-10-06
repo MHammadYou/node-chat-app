fed2430b3adda1eddf37f08b63bb8f77708ccbc7
MHammadYou <MHammadY2.0@gmail.com>
2023-10-06 18:42:56 +0500

    Add SignupPage

---
+++ ui/src/pages/SignupPage.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const SignupPage: React.FC = () => {
+  return (
+    <Box>
+      <Typography>Signup Page</Typography>
+    </Box>
+  );
+};
+
+export default SignupPage;
