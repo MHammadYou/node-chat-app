77a8f384f1a76f2f0c4f216e2756600f4fe5728f
MHammadYou <MHammadY2.0@gmail.com>
2023-10-07 18:43:57 +0500

    Add SignupPage exports

--- ui/src/pages/index.ts
+++ ui/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as HomePage } from "./HomePage";
 export { default as ChatPage } from "./ChatPage";
 export { default as LoginPage } from "./LoginPage";
+export { default as SignupPage } from "./SignupPage";
