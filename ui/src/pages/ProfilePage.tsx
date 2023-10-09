fce212dccee7b91b0fd2aa60818b3213de0a381e
MHammadYou <MHammadY2.0@gmail.com>
2023-10-09 18:47:12 +0500

    Add ProfilePage

---
+++ ui/src/pages/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Box>
+      <Typography>Profile Page</Typography>
+    </Box>
+  );
+};
+
+export default ProfilePage;
