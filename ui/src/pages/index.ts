0bc9e2edd286061e832dbb1a74ba51389e21b4dd
MHammadYou <MHammadY2.0@gmail.com>
2023-10-10 18:47:56 +0500

    Add ProfilePage exports

--- ui/src/pages/index.ts
+++ ui/src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as HomePage } from "./HomePage";
 export { default as ChatPage } from "./ChatPage";
 export { default as LoginPage } from "./LoginPage";
 export { default as SignupPage } from "./SignupPage";
+export { default as ProfilePage } from "./ProfilePage";
