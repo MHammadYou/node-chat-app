c1d89a4d76df514d5c7f3f51276b3cf6330662ec
MHammadYou <MHammadY2.0@gmail.com>
2023-10-18 19:22:36 +0500

    Relocate Router to Routes.tsx

--- ui/src/components/app/App.tsx
+++ ui/src/components/app/App.tsx
@@ -1,40 +1,15 @@
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from "store/";
 import ThemeProvider from "theme/ThemeProvider";
 
-import Root from "./Root";
-import {
-  ChatPage,
-  HomePage,
-  LoginPage,
-  ProfilePage,
-  SignupPage,
-} from "../../pages";
+import Routes from "./Routes";
 
 const App: React.FC = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index element={<HomePage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Route>
-    )
-  );
-
   return (
     <Provider store={store}>
       <ThemeProvider>
-        <RouterProvider router={router} />
+        <Routes />
       </ThemeProvider>
     </Provider>
   );
--- ui/src/components/app/Routes.tsx
+++ ui/src/components/app/Routes.tsx
@@ -1,5 +1,34 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+import {
+  HomePage,
+  ChatPage,
+  LoginPage,
+  SignupPage,
+  ProfilePage,
+} from "src/pages";
+
+import Root from "./Root";
+
 const Routes: React.FC = () => {
-  return <>Routes Here</>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 };
 
 export default Routes;
