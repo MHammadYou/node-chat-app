1c0aa3093e0df0a048b438aded95b0bddecf90e2
MHammadYou <MHammadY2.0@gmail.com>
2023-10-25 00:59:18 +0500

    Update default Route

--- ui/src/components/app/RoutesProvider.tsx
+++ ui/src/components/app/RoutesProvider.tsx
@@ -5,15 +5,13 @@ import {
   Route,
 } from "react-router-dom";
 
-import ROUTES from "constants/routes.ts";
-
 import Root from "./Root";
 import { routes } from "./Routes";
 
 const RoutesProvider: React.FC = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path={ROUTES.default} element={<Root />}>
+      <Route path="/" element={<Root />}>
         {routes.map(({ path, component }) => (
           <Route path={path} element={component} />
         ))}
