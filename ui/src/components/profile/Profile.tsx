fb71041b1742180fd3bade440f62628aae42c317
MHammadYou <MHammadY2.0@gmail.com>
2023-10-29 01:03:30 +0500

    Add Profile

---
+++ ui/src/components/profile/Profile.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Profile: React.FC = () => {
+  return (
+    <Box>
+      <Typography>Profile</Typography>
+    </Box>
+  );
+};
+
+export default Profile;
