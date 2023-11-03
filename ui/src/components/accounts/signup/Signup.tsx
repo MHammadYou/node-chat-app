2f060486fe427752c866a27b8f0c702a6e6f8fc0
MHammadYou <MHammadY2.0@gmail.com>
2023-11-04 01:08:43 +0500

    Add Signup

---
+++ ui/src/components/accounts/signup/Signup.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Signup: React.FC = () => {
+  return (
+    <Box>
+      <Typography>Signup</Typography>
+    </Box>
+  );
+};
+
+export default Signup;
