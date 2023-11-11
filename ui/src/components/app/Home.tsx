b391d078a1f5d0afd070392891acee37f99653d8
MHammadYou <MHammadY2.0@gmail.com>
2023-11-12 02:16:04 +0500

    Add Home Component

---
+++ ui/src/components/app/Home.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Home: React.FC = () => {
+  return (
+    <Box>
+      <Typography>Chat APP</Typography>
+    </Box>
+  );
+};
+
+export default Home;
