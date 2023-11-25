68253a156c48e2764710da1e555c000053522aa5
MHammadYou <MHammadY2.0@gmail.com>
2023-11-25 23:22:27 +0500

    Add db connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 import handleRoutes from "./routes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const DB_URI = process.env.DB_URI || "";
 
 app.use(express.json());
 
-app.listen(PORT, () => `Listening on port: ${PORT}`);
+(async () => {
+  try {
+    const res = await mongoose.connect(DB_URI);
+
+    if (res) {
+      console.log("Connected to the database");
+      app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 handleRoutes(app);
