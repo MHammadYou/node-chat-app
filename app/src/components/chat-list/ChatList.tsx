7cefcee9130503437691e0a2b4ad263fefede4c4
MHammadYou <MHammadY2.0@gmail.com>
2024-03-14 09:13:26 +0500

    format last message

--- app/src/components/chat-list/ChatList.tsx
+++ app/src/components/chat-list/ChatList.tsx
@@ -9,8 +9,10 @@ import {
 } from "@mui/material";
 import { Image } from "@mui/icons-material";
 
-import { useGetChatsQuery } from "store/api/chat";
 import Loading from "lib/Loading";
+
+import useUser from "hooks/useUser";
+import { useGetChatsQuery } from "store/api/chat";
 
 type Props = {
   setSelectedChat: React.Dispatch<React.SetStateAction<string>>;
@@ -19,8 +21,20 @@ type Props = {
 const ChatList: React.FC<Props> = ({ setSelectedChat }) => {
   const { data: chats, isLoading, error } = useGetChatsQuery();
 
-  const formatMessage = (text: string, maxLength = 32) =>
+  const user = useUser();
+  const isOwnMessage = (username?: string) => username === user;
+
+  const formatMessageLength = (text: string, maxLength = 32) =>
     text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
+
+  const formatMessage = (isGroup: boolean, text?: string, username?: string) =>
+    `${
+      text
+        ? `${
+            isGroup ? (isOwnMessage(username) ? "You" : username) + ": " : ""
+          }${formatMessageLength(text)}`
+        : "No message yet"
+    }`;
 
   if (isLoading) return <Loading />;
   if (error) return <div>{error.toString()}</div>;
@@ -29,27 +43,25 @@ const ChatList: React.FC<Props> = ({ setSelectedChat }) => {
 
   return (
     <List sx={{ width: "100%", bgcolor: "background.paper" }} disablePadding>
-      {chats.map(({ id, isGroup, lastMessage, name }) => (
-        <Box key={id}>
-          <ListItem onClick={() => setSelectedChat(id)}>
-            <ListItemAvatar>
-              <Avatar>
-                <Image />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary={name || "TODO: Add chat name"}
-              secondary={
-                lastMessage
-                  ? `${isGroup ? lastMessage.username + ": " : ""}
-                    ${formatMessage(lastMessage.text)}`
-                  : "No messages yet"
-              }
-            />
-          </ListItem>
-          <Divider />
-        </Box>
-      ))}
+      {chats.map(({ id, isGroup, lastMessage, name }) => {
+        const { username, text } = lastMessage || {};
+        return (
+          <Box key={id}>
+            <ListItem onClick={() => setSelectedChat(id)}>
+              <ListItemAvatar>
+                <Avatar>
+                  <Image />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={name || "TODO: Add chat name"}
+                secondary={formatMessage(isGroup, text, username)}
+              />
+            </ListItem>
+            <Divider />
+          </Box>
+        );
+      })}
     </List>
   );
 };
